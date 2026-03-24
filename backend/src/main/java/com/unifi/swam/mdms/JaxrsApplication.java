adc98818868ceeb6e1aa7522d3bcfc91ff9f4c5d
Shahzeb Salim <34137097+ShahzebSalim@users.noreply.github.com>
2026-03-24 10:20:45 +0100

    Update JaxrsApplication.java with new content

--- backend/src/main/java/com/unifi/swam/mdms/JaxrsApplication.java
+++ backend/src/main/java/com/unifi/swam/mdms/JaxrsApplication.java
@@ -1,9 +1,8 @@
 package com.unifi.swam.mdms;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
 public class JaxrsApplication extends Application {
-    // Custom configuration can go here if needed
 }
