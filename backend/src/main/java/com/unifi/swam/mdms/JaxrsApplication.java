962eaa3bcdaf83690691b2681ec2e94e8cbb2093
Shahzeb Salim <34137097+ShahzebSalim@users.noreply.github.com>
2026-03-24 10:19:10 +0100

    Add JaxrsApplication.java

---
+++ backend/src/main/java/com/unifi/swam/mdms/JaxrsApplication.java
@@ -0,0 +1,9 @@
+package com.unifi.swam.mdms;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JaxrsApplication extends Application {
+    // Custom configuration can go here if needed
+}
